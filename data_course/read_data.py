abf2386641429d211c8b4a143b62eb66a58ce5ad
celinevw <celinejuoching@gmail.com>
2023-03-09 14:52:32 +0100

    Store frequencies of outcomes for all replicates in 1 file

--- data_course/read_data.py
+++ data_course/read_data.py
@@ -6,14 +6,14 @@ import numpy as np
 # First column is the guide sequence, the next 3033 columns are the features(2649 MH binary + 384 one-hot encoded features) 
 # and the last 557 columns are the observed outcome(class) frequencies.
 
-Lindel_training = pd.read_csv("data_course/Lindel_training.txt", sep='\t')
+Lindel_training = pd.read_csv("Lindel_training.txt", sep='\t')
 
-label, rev_index, features = pkl.load(open('data_course/feature_index_all.pkl','rb'))
+label, rev_index, features = pkl.load(open('feature_index_all.pkl','rb'))
 
 
-matrix1 = pkl.load(open('data_course/NHEJ_rep1_final_matrix.pkl','rb'))
-matrix2 = pkl.load(open('data_course/NHEJ_rep2_final_matrix.pkl','rb'))
-matrix3 = pkl.load(open('data_course/NHEJ_rep3_final_matrix.pkl','rb'))
+matrix1 = pkl.load(open('NHEJ_rep1_final_matrix.pkl','rb'))
+matrix2 = pkl.load(open('NHEJ_rep2_final_matrix.pkl','rb'))
+matrix3 = pkl.load(open('NHEJ_rep3_final_matrix.pkl','rb'))
 
 ### FIGURE 2B - VIOLIN PLOT PROCESSING ###
 
@@ -24,15 +24,19 @@ frequency_repl1 = {}
 frequency_repl2 = {}
 frequency_repl3 = {}
 
+frequency_comb = {}
 # initialize frequency array
 for i in range(len(matrix1)):
     frequency_repl1[matrix1[i][2]] = {}
+    frequency_comb[matrix1[i][2]] = {}
 
 for i in range(len(matrix2)):
     frequency_repl2[matrix2[i][2]] = {}
+    frequency_comb[matrix2[i][2]] = {}
 
 for i in range(len(matrix3)):
     frequency_repl3[matrix3[i][2]] = {}
+    frequency_comb[matrix3[i][2]] = {}
 
 # count frequencies of different alleles per target
 for i in range(len(matrix1)):
@@ -42,12 +46,22 @@ for i in range(len(matrix1)):
         else:
             frequency_repl1[matrix1[i][2]][matrix1[i][3]] += 1
 
+        if matrix1[i][3] not in frequency_comb[matrix1[i][2]]:
+            frequency_comb[matrix1[i][2]][matrix1[i][3]] = [1, 0, 0]
+        else:
+            frequency_comb[matrix1[i][2]][matrix1[i][3]][0] += 1
+
 for i in range(len(matrix2)):
     if matrix2[i][2] != matrix2[i][3]:
         if matrix2[i][3] not in frequency_repl2[matrix2[i][2]]:
             frequency_repl2[matrix2[i][2]][matrix2[i][3]] = 1
         else:
             frequency_repl2[matrix2[i][2]][matrix2[i][3]] += 1
+
+        if matrix2[i][3] not in frequency_comb[matrix2[i][2]]:
+            frequency_comb[matrix2[i][2]][matrix2[i][3]] = [0, 1, 0]
+        else:
+            frequency_comb[matrix2[i][2]][matrix2[i][3]][1] += 1
 
 for i in range(len(matrix3)):
     if matrix3[i][2] != matrix3[i][3]:
@@ -56,14 +70,24 @@ for i in range(len(matrix3)):
         else:
             frequency_repl3[matrix3[i][2]][matrix3[i][3]] += 1
 
-with open("data_course/repl1_frequency_data.txt", 'w') as file:
+        if matrix3[i][3] not in frequency_comb[matrix3[i][2]]:
+            frequency_comb[matrix3[i][2]][matrix3[i][3]] = [0, 0, 1]
+        else:
+            frequency_comb[matrix3[i][2]][matrix3[i][3]][2] += 1
+
+with open("repl1_frequency_data.txt", 'w') as file:
     for key in frequency_repl1:
         file.write(key + ": " + str(frequency_repl1[key]) + "\n")
 
-with open("data_course/repl2_frequency_data.txt", 'w') as file:
+with open("repl2_frequency_data.txt", 'w') as file:
     for key in frequency_repl2:
         file.write(key + ": " + str(frequency_repl2[key]) + "\n")
 
-with open("data_course/repl3_frequency_data.txt", 'w') as file:
+with open("repl3_frequency_data.txt", 'w') as file:
     for key in frequency_repl3:
         file.write(key + ": " + str(frequency_repl3[key]) + "\n")
+
+with open("combi_frequency_data.txt", 'w') as file:
+    for key in frequency_comb:
+        file.write(key + ": " + str(frequency_comb[key]) + "\n")
+
