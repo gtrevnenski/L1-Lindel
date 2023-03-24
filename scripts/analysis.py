36cf50341ec1d388d74410076a012572fb3f27f6
Georgi Trevnenski <G.D.Trevnenski-1@student.tudelft.nl>
2023-03-24 15:32:18 +0100

    setup for testing

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -2,9 +2,10 @@ from tensorflow import keras
 import pandas as pd
 import numpy as np
 from ast import literal_eval
-import pickle as pkl
+import pickle
 import utils
-
+from Lindel.Predictor import *
+import os
 
 def find_seq_and_replace(data_set, test_seq, sequences_60bp, test_data_point):
     for seq in data_set:
@@ -31,10 +32,8 @@ def get_test_set_60bp():
                 row.append(float(token))
             test_data.append(row)
 
-
-    seventy_k, homing_design, mh1_200bp, _, _, _ = utils.read_200bp_sequences('../data_course/algient_NHEJ_guides_final.txt')
-    combined_set = seventy_k + mh1_200bp
-    print(len(combined_set))
+    seventy_k, homing_design, mh1_200bp, _, _, _ = utils.read_200bp_sequences(
+        '../data_course/algient_NHEJ_guides_final.txt')
     sequences_60bp = []
 
     for test_data_point in test_data:
@@ -46,11 +45,45 @@ def get_test_set_60bp():
         else:
             find_seq_and_replace(mh1_200bp, test_seq, sequences_60bp, test_data_point)
 
-    print("\n-------------\n")
-    print(test_data[0][0])
-    print(len(test_data[0][0]))
-    print(len(sequences_60bp))#440 data points in Lindel_test
+    # print("\n-------------\n")
+    # print(test_data[0][0])
+    # print(len(test_data[0][0]))
+    # print(len(sequences_60bp))  # 440 data points in Lindel_test
+
+    return test_data
+
+
+def mse(x, y):
+    return ((x-y)**2).mean()
 
 
 if __name__ == '__main__':
     get_test_set_60bp()
+
+    # model = keras.models.load_model('C:\TUDelft\BioInformatics\MLB\L1-Lindel\data\L1_del.h5')
+    # layers = model.layers
+    # for layer in model.layers[1]:
+    #     weights = layer.get_weights()
+    #     # bias = layer.get_bias()
+    #     print(weights)
+    # print("here")
+    #
+    # with open('C:\TUDelft\BioInformatics\MLB\L1-Lindel\data_course\\feature_index_all.pkl', 'rb') as f:
+    #     data = pkl.load(f)
+    #     print("here")
+
+    test_data = get_test_set_60bp()
+    model_del_array_weights, model_del_array_biases, model_ratio_array_weights, model_ratio_array_biases, model_ins_array_weights, model_ins_array_biases = utils.get_weights_biases()
+    weights_biases = [model_ratio_array_weights, model_ratio_array_biases, model_del_array_weights, model_del_array_biases,
+         model_ins_array_weights, model_ins_array_biases]
+
+    prerequesites = pickle.load('../data_course/model_prereq.pkl'), 'rb'))
+
+
+    for test_point in test_data:
+        test_seq = test_point[0]
+        gen_prediction(test_seq, weights_biases, )
+
+
+
+
