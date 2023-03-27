1af9c28234c3581a336d2199d30ab481af4c6431
celinevw <celinejuoching@gmail.com>
2023-03-27 14:35:27 +0200

    Fix mistake in histogram plot and format file nicely

--- scripts/Logistic model training/LR_aggregate.py
+++ scripts/Logistic model training/LR_aggregate.py
@@ -56,16 +56,17 @@ def onehotencoder(seq):
 # Load data
 #threshold = sys.argv[1]
 
-"""
 
+"""
 ''' Load the counts for all targets. '''
+# This was used to make a histogram for the Aggregate model where the frequencies of a target are scaled by the number
+# of UMIs were counted for that target. This was done to explore if the figure would become more similar doing this.
 target_to_count = {}
 with open("../../data_course/combi_totalfrequency.txt", 'r') as file:
     for line in file:
         target_to_count[line.split(",")[0]] = int(line.split(",")[1])
 """
 
-"""
 ''' This part loads the training data. '''
 workdir  = "C:/Users/celin/PycharmProjects/L1-Lindel/data/"
 fname    = "Lindel_training.txt"
@@ -76,19 +77,16 @@ data     = np.loadtxt(workdir+fname, delimiter="\t", dtype=str)
 Seqs = data[:,0]
 data = data[:,1:].astype('float32')
 
-# Sum up deletions and insertions to 
 ''' The first 'feature_size' values are inputs, all others are output. The Aggregate Model will output the average
 frequency of each class, independently of the input. The frequencies are stored to a numpy array file. '''
-
-X = data[:,:feature_size]
-y = data[:, feature_size:]
+y_truth = data[:, feature_size:]
 
 '''
-# Here we scale the frequencies by the number of counts for a target
+# For exploration: scaling the frequencies by the number of counts for a target.
 y_scaled = []
-for i in range(y.shape[0]):
+for i in range(y_truth.shape[0]):
     counts = target_to_count[Seqs[i]] if (Seqs[i] in target_to_count) else 0
-    y_scaled.append(y[i,:]*counts)
+    y_scaled.append(y_truth[i,:]*counts)
 
 y_scaled = np.array(y_scaled)
 y_summed = np.sum(y_scaled, axis=0) # sum over all targets to find counts per class
@@ -97,13 +95,13 @@ agg_output_scaled = y_summed / np.sum(y_summed) #
 np.save("agg_output_scaled", agg_output_scaled)
 '''
 
-agg_output = np.mean(y, axis=0)
+agg_output = np.mean(y_truth, axis=0)
 np.save("agg_output", agg_output)
 
-''' This part computes the MSE for the Aggregate Model.
+''' This part computes the MSE for Lindel and the Aggregate Model.
 We load the test data in "Lindel_test.txt". The first 'feature_size' values are inputs, all others are output. For each
-of the input items in the data, we compute the MSE between the output and the output of the Aggregate Model. These
-values are collected and visualized in a histogram. '''
+of the input items in the data, we compute the MSE between the output and the output of Lindel or the Aggregate Model.
+These values are collected and visualized in a histogram. '''
 
 workdir  = "C:/Users/celin/PycharmProjects/L1-Lindel/data/"
 fname    = "Lindel_test.txt"
@@ -115,23 +113,34 @@ Seqs = data[:,0]
 data = data[:,1:].astype('float32')
 
 X = data[:,:feature_size]
-y = data[:, feature_size:]
-print('y.shape = ',y.shape)
+y_test = data[:, feature_size:]
 
-agg_output = np.load("agg_output.npy")
-MSE = np.zeros(y.shape[0])
-for i in range(y.shape[0]):
-    MSE[i] = mse(agg_output,y[i,:])
+# Load Aggregate Model outputs if not running the whole script
+# agg_output = np.load("agg_output.npy")
+MSE_aggregate = np.zeros(y_test.shape[0])
+for i in range(y_test.shape[0]):
+    MSE_aggregate[i] = mse(agg_output, y_test[i, :])
 
-np.save("MSE_aggregate", MSE)
-"""
+# Load Lindel Outputs
+lindel_output = np.load("../../lindel_output.npy")
+MSE_lindel = np.zeros(y_test.shape[0])
+for i in range(y_test.shape[0]):
+    MSE_lindel[i] = mse(y_test[i, :], lindel_output[i,:])
+
+np.save("MSE_aggregate", MSE_aggregate)
+np.save("MSE_lindel", MSE_lindel)
 
 ''' This part makes a histogram of the MSEs. '''
-MSE = np.load("MSE_aggregate.npy")
+# MSE_aggregate = np.load("MSE_aggregate.npy")
+# MSE_lindel = np.load("MSE_lindel.npy")
+
 fig, ax = plt.subplots()
-ax.hist(MSE*1000, bins=25, alpha=0.5)
+ax.hist(MSE_lindel*1000, bins=40, alpha=0.3, label="Lindel")
+ax.hist(MSE_aggregate*1000, bins=25, alpha=0.2, color='r', label="Aggregate Model")
 ax.set_xlabel ("MSE (x10^-3)")
 ax.set_ylabel("Counts")
-ax.set_title("Performance of the Aggregate Model on the test set")
+ax.set_title("Performance of Lindel and the Aggregate Model on the test set")
+plt.legend()
 plt.show()
-plt.savefig('../../Figures/Aggregate.eps', format='eps')
+plt.savefig('../../Figures/ModelPerformance.eps', format='eps')
+plt.savefig('../../Figures/ModelPerformance.png', format='png')
